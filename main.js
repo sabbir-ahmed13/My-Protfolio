4dacd5efe2eb7544fe05fbfa4de47015e18c3e0d
Sabbir Ahmed <133645137+sabbir-ahmed13@users.noreply.github.com>
2023-10-30 20:59:19 +0600

    Added index.html, style.css, main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    typespeed: 100,
+    Backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
